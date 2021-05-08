a6f4d8c559a2bd67e98a2531998e63b2ed672135
Jorge Viramontes <>
2021-05-08 15:15:09 -0500

    Added header css module + styling for header, header title, and header links. Code cleanup in header.js (removed in-line styles and setting class name. SO MUCH cleaner 🔥)

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,21 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import * as headerStyles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div
-      style={{
-        margin: `0 auto`,
-        height: `200px`,
-        padding: `1.45rem 3rem`,
-        backgroundColor: `#3CACAE`, // add somewhere as a constant
-        color: `#C8F4F9`, // add somewhere as a constant
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `#C8F4F9`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header className={headerStyles.header}>
+    <div>
+      <h1>
+        <Link to="/" className={headerStyles.headerTitle}>{siteTitle}</Link>
       </h1>
 
       <ul>
-        <li>
-          <Link to="/about">About Me</Link>
-          </li>
-        <li>
-          <Link to="/blog">My Blog</Link>
-          </li>
-        <li>
-        <Link to="/contact-me">Contact Me</Link>
-          </li>
+        <li> <Link to="/about" className={headerStyles.headerLink}>About Me</Link> </li>
+        <li> <Link to="/blog" className={headerStyles.headerLink}>My Blog</Link> </li>
+        <li> <Link to="/contact-me" className={headerStyles.headerLink}>Contact Me</Link> </li>
       </ul>
     </div>
-    <div ></div>
   </header>
 )
 
