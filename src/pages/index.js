5b513b9595b78aaaf84f3afa19e6c3d7ba0a5f74
Jorge Viramontes <>
2021-05-22 11:11:43 -0500

    Added open sans to h2. Added additional copy to home page, added component for an IconCollection that currently contains some H2s. Added some styling to the iconCollection.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,31 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import IconCollection from "../components/iconCollection"
 
 const IndexPage = () => (  
   <Layout>
     <SEO title="Home" />
-    <h1>Coming Soon...</h1>
-    <p>Thank you for visiting my website.</p>
-    <p>I hope you have fun while you're here.</p>
-    <p>There's plenty more coming your way, and this is only the beginning.</p>
+    <h1>Welcome to my website.</h1>
+    <h2>Hi!</h2>
+    <p>This is a website I’ve built completely from the ground up.</p>
+
+    <p>Here are just some of the different technologies I used to build this site:</p>
+    <IconCollection></IconCollection>
+    
+    <ul>
+      <li>No heavy bogged down WordPress integrations or generic pre-built themes.</li>
+      <li>Customize as much as I want.</li>
+      <li>Quickly deploy updates to the cooud as soon as I merge to the main branch.</li>
+      <li>It's all open source, and you can check it out here.</li>
+    </ul>
+
+    <h2>Thanks for visiting!</h2>
+    
+    <h1>A Little More About Me</h1>
+
     <StaticImage
       src="../images/gatsby-icon.png"
       width={140}
