84fc4fd23dab442072a1fdfbf09fd1beb9c559be
Jorge Viramontes <>
2021-05-27 22:35:14 -0500

    Separated content in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,24 +7,28 @@ const Footer = () => (
 
     <footer className={styles.footer}>
         <div>
-        <p className={styles.poweredBy}>Powered By:</p>
-        <a href="https://www.alacrandigitalservices.com/" target="_blank">
-            <StaticImage
-                src="../images/white2.png" alt="Alacran Logo"
-                placeholder="blurred"
-                layout="fixed"
-                height={80}
-                formats={["AUTO", "WEBP", "AVIF"]}
-            />
-        </a>
+            <p className={styles.poweredBy}>Powered By:</p>
+            <a href="https://www.alacrandigitalservices.com/" target="_blank">
+                <StaticImage
+                    src="../images/white2.png" alt="Alacran Logo"
+                    placeholder="blurred"
+                    layout="fixed"
+                    height={80}
+                    formats={["AUTO", "WEBP", "AVIF"]}
+                />
+            </a>
         </div>
-        
+
         <div>
-        <a href="https://www.alacrandigitalservices.com/" target="_blank">Alacrán Digital Services</a>{` `} © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+            <a href="https://www.alacrandigitalservices.com/" target="_blank">Alacrán Digital Services</a>{` `} © {new Date().getFullYear()}
         </div>
-        
+
+        <div>
+            Built with
+        {` `}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </div>
+
     </footer>
 )
 
