3ad5dda5b2507c0d21926da819ff57acfe48e9c2
Jorge Viramontes <>
2021-06-16 18:28:55 -0500

    adding / before pages

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -13,7 +13,7 @@ const ContactPage = () => (
             Please fill out this short form and I will respond as soon as I can (usually within 2 business days).
         </p>
 
-        <form name="contact" method="post" data-netlify="true" action="pages/contact-success" data-netlify-honeypot="bot-field">
+        <form name="contact" method="post" data-netlify="true" action="/pages/contact-success" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact" />
             <p>
                 <label>First Name: <input type="text" name="firstName" /></label>
