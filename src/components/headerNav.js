de108f2f0cb00773f7376b04bfb7fa3dcf1ea7b3
Jorge Viramontes <>
2021-06-19 19:37:25 -0500

    Make header bottom border thinner, make icon size smaller, modify header nav to accommodate headerLinkComponents at different sizes. Add a border for desktop to look like buttons. Add header link padding of 0.5rem.

--- src/components/headerNav.js
+++ src/components/headerNav.js
@@ -11,18 +11,18 @@ const HeaderNav = () => (
     <div className={headerNavStyles.headerNav}>
 
         <div className={headerNavStyles.headerLinkContainer}>
-            <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "2rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiHome></FiHome></span>
             </IconContext.Provider>
             <Link to="/" className={headerNavStyles.headerLink}>Home</Link>
         </div>
 
         <div className={headerNavStyles.headerLinkContainer}>
-            <IconContext.Provider value={{ color: "#ADD8E6", size: "2.5rem" }}>
+            <IconContext.Provider value={{ color: "#ADD8E6", size: "2rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiMail></FiMail></span>
             </IconContext.Provider>
 
-            <Link to="/contact-me" className={headerNavStyles.headerLink}>Send Message</Link>
+            <Link to="/contact-me" className={headerNavStyles.headerLink}>Message</Link>
         </div>
 
         
@@ -31,14 +31,14 @@ const HeaderNav = () => (
         GALLERY & SHOP ARE FUTURE FEATURES.
         
         <div className={headerNavStyles.headerLinkContainer}>
-            <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "2rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiCamera></FiCamera></span>
             </IconContext.Provider>
             <Link to="/" className={headerNavStyles.headerLink}>Gallery</Link>
         </div>
 
         <div className={headerNavStyles.headerLinkContainer}>
-            <IconContext.Provider value={{ color: "#56C6C6", size: "2.5rem" }}>
+            <IconContext.Provider value={{ color: "#56C6C6", size: "2rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiShoppingBag></FiShoppingBag></span>
             </IconContext.Provider>
             <Link to="/" className={headerNavStyles.headerLink}>Shop</Link>
@@ -47,14 +47,14 @@ const HeaderNav = () => (
         */}
 
         <div className={headerNavStyles.headerLinkContainer}>
-            <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "2rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiBook></FiBook></span>
             </IconContext.Provider>
             <Link to="/blog" className={headerNavStyles.headerLink}>Blog</Link>
         </div>
 
         <div className={headerNavStyles.headerLinkContainer}>
-            <IconContext.Provider value={{ color: "#ADD8E6", size: "2.5rem" }}>
+            <IconContext.Provider value={{ color: "#ADD8E6", size: "2rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiUser></FiUser></span>
             </IconContext.Provider>
             <Link to="/about" className={headerNavStyles.headerLink}>About Me</Link>
