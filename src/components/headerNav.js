6655f45ea2fdf54b6d44470a61ca3f5f7b0d43d8
Jorge Viramontes <>
2021-06-19 19:03:41 -0500

    Comment out Gallery & Shop as they aren't currently existing pages, add working links to active nav links.

--- src/components/headerNav.js
+++ src/components/headerNav.js
@@ -22,9 +22,14 @@ const HeaderNav = () => (
                 <span className={headerNavStyles.headerLinkIcon}><FiMail></FiMail></span>
             </IconContext.Provider>
 
-            <Link to="/" className={headerNavStyles.headerLink}>Send Message</Link>
+            <Link to="/contact-me" className={headerNavStyles.headerLink}>Send Message</Link>
         </div>
 
+        
+        {/* 
+        
+        GALLERY & SHOP ARE FUTURE FEATURES.
+        
         <div className={headerNavStyles.headerLinkContainer}>
             <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiCamera></FiCamera></span>
@@ -37,20 +42,22 @@ const HeaderNav = () => (
                 <span className={headerNavStyles.headerLinkIcon}><FiShoppingBag></FiShoppingBag></span>
             </IconContext.Provider>
             <Link to="/" className={headerNavStyles.headerLink}>Shop</Link>
-        </div>
+        </div> 
+        
+        */}
 
         <div className={headerNavStyles.headerLinkContainer}>
             <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiBook></FiBook></span>
             </IconContext.Provider>
-            <Link to="/" className={headerNavStyles.headerLink}>Blog</Link>
+            <Link to="/blog" className={headerNavStyles.headerLink}>Blog</Link>
         </div>
 
         <div className={headerNavStyles.headerLinkContainer}>
             <IconContext.Provider value={{ color: "#ADD8E6", size: "2.5rem" }}>
                 <span className={headerNavStyles.headerLinkIcon}><FiUser></FiUser></span>
             </IconContext.Provider>
-            <Link to="/" className={headerNavStyles.headerLink}>About Me</Link>
+            <Link to="/about" className={headerNavStyles.headerLink}>About Me</Link>
         </div>
     </div>
 
