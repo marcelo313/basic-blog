e184cbb3c9086af16721a095e5187b62574f5cf5
Jorge Viramontes <>
2021-06-19 19:00:52 -0500

    Add react icons to project, Create HeaderNav with 6 HeaderLinkContainers each with an icon and a Link.

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,12 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import * as headerStyles from "./header.module.css"
 
+import HeaderNav from "./headerNav"
+
 const Header = ({ siteTitle }) => (
   <header className={headerStyles.header}>
     <Link to="/" className={headerStyles.headerTitle}>{siteTitle}</Link>
-    <div className={headerStyles.headerNav}>
-      <Link to="/about" className={headerStyles.headerLink}>About</Link>
-      <Link to="/blog" className={headerStyles.headerLink}>Blog</Link>
-      <Link to="/contact-me" className={headerStyles.headerLink}>Let's Connect</Link>
-    </div>
+    <HeaderNav></HeaderNav>
   </header>
 )
 
---
+++ src/components/headerNav.js
@@ -0,0 +1,59 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import * as headerNavStyles from "./headerNav.module.css"
+
+import { IconContext } from "react-icons"
+
+import { FiMail, FiCamera, FiHome, FiShoppingBag, FiBook, FiUser } from 'react-icons/fi'
+
+const HeaderNav = () => (
+
+    <div className={headerNavStyles.headerNav}>
+
+        <div className={headerNavStyles.headerLinkContainer}>
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
+                <span className={headerNavStyles.headerLinkIcon}><FiHome></FiHome></span>
+            </IconContext.Provider>
+            <Link to="/" className={headerNavStyles.headerLink}>Home</Link>
+        </div>
+
+        <div className={headerNavStyles.headerLinkContainer}>
+            <IconContext.Provider value={{ color: "#ADD8E6", size: "2.5rem" }}>
+                <span className={headerNavStyles.headerLinkIcon}><FiMail></FiMail></span>
+            </IconContext.Provider>
+
+            <Link to="/" className={headerNavStyles.headerLink}>Send Message</Link>
+        </div>
+
+        <div className={headerNavStyles.headerLinkContainer}>
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
+                <span className={headerNavStyles.headerLinkIcon}><FiCamera></FiCamera></span>
+            </IconContext.Provider>
+            <Link to="/" className={headerNavStyles.headerLink}>Gallery</Link>
+        </div>
+
+        <div className={headerNavStyles.headerLinkContainer}>
+            <IconContext.Provider value={{ color: "#56C6C6", size: "2.5rem" }}>
+                <span className={headerNavStyles.headerLinkIcon}><FiShoppingBag></FiShoppingBag></span>
+            </IconContext.Provider>
+            <Link to="/" className={headerNavStyles.headerLink}>Shop</Link>
+        </div>
+
+        <div className={headerNavStyles.headerLinkContainer}>
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "2.5rem" }}>
+                <span className={headerNavStyles.headerLinkIcon}><FiBook></FiBook></span>
+            </IconContext.Provider>
+            <Link to="/" className={headerNavStyles.headerLink}>Blog</Link>
+        </div>
+
+        <div className={headerNavStyles.headerLinkContainer}>
+            <IconContext.Provider value={{ color: "#ADD8E6", size: "2.5rem" }}>
+                <span className={headerNavStyles.headerLinkIcon}><FiUser></FiUser></span>
+            </IconContext.Provider>
+            <Link to="/" className={headerNavStyles.headerLink}>About Me</Link>
+        </div>
+    </div>
+
+)
+
+export default HeaderNav
