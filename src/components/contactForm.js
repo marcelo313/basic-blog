badd33b93e81fe4b043b8c168a76339dac7c854a
marcewo <marcewo@umich.edu>
2021-07-02 14:03:49 -0400

    I made it look pretty

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -2,23 +2,34 @@ import * as React from "react"
 import * as styles from "./contactForm.module.css"
 
 const ContactForm = () => (
-    <form className={styles.formContainer} name="contact" method="post" data-netlify="true" action="/contact-success" data-netlify-honeypot="bot-field">
+    <form className={styles.formContainer} name="contact" method="post" data-netlify="true" action="pages/contact-success" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact" />
         <div className={styles.nameContainer}>
-            <p>
-                <label>First Name: <input type="text" name="firstName" /></label>
-            </p>
-            <p>
-                <label>Last Name: <input type="text" name="lastName" /></label>
-            </p>
+            <div className={styles.inputGroup}>
+                <label htmlFor="firstName">First Name * </label>
+                <input type="text" name="firstName" style={{ width: `300px` }}
+                    required />
+            </div>
+
+            <div className={styles.inputGroup}>
+                <label htmlFor="lastName">Last Name * </label>
+                <input type="text" name="lastName" style={{ width: `300x` }}
+                    required />
+            </div>
         </div>
 
-        <p>
-            <label>Email: <input type="email" name="emailAddress" size="30" /></label>
+
+
+
+
+        <p className={styles.inputGroup}>
+            <label htmlFor="email"> Email * </label>
+            <input type="email" name="emailAddress" style={{ width: `310px` }}
+                required />
         </p>
         <p>
-            <label>Message: <br></br> <textarea name="message" className={styles.formMessage}></textarea></label> 
-            
+            <label>Message <br></br> <textarea name="message" className={styles.formMessage}></textarea></label>
+
         </p>
         <p>
             <button type="submit">Send</button>
